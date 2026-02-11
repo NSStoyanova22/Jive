4d9283ca2ed5ccf6078020469c6af35ac394b9d6
nsstoyanova22 <nsstoyanova22@codingburgas.bg>
2026-02-11 16:26:12 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/jive/JiveApplication.java
@@ -0,0 +1,13 @@
+package com.jive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiveApplication.class, args);
+	}
+
+}
