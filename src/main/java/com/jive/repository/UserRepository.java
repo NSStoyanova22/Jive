6ff3d0e9f15e9703029a18193a9c55d7509ac553
nsstoyanova22 <nsstoyanova22@codingburgas.bg>
2026-02-11 21:31:14 +0200

    Add findByUsername method to UserRepository for user retrieval by username

--- src/main/java/com/jive/repository/UserRepository.java
+++ src/main/java/com/jive/repository/UserRepository.java
@@ -3,4 +3,8 @@ package com.jive.repository;
 import com.jive.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
